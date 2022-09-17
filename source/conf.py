105fd867e632f982b1607a5f37a5e0a727058057
Victor Férat <victor.ferat@live.Fr>
2022-09-17 17:30:35 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -9,6 +9,7 @@
 project = 'demo'
 copyright = '2022, Victor Férat'
 author = 'Victor Férat'
+github_user = "vferat"
 github_project = 'sphinx_collab_demo'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -27,10 +28,13 @@ html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
-    "repository_url": f"https://github.com/{github_project}",
+    "repository_url": github_user + "/" + github_project,
+    "path_to_docs": "source/",
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
+    "repository_branch": "main",
+    #"launch_buttons": True, # Add binder to jupyter notebook pages
 }
 
 # Extensions
